dea8934d37d6e5a56110edda3320dfc3123eb9f6
AdrianaAvila1710p <127752945+AdrianaAvila1710p@users.noreply.github.com>
2024-09-14 19:49:28 -0300

    aaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'uno',
+    loadComponent: () => import('./uno/uno.page').then( m => m.UnoPage)
+  },
 ];
